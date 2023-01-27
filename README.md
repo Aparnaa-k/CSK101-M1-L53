d1d4ce3baf796a65ac00107e6f7bba7e3062e55d
Aparnaa-k <119048424+Aparnaa-k@users.noreply.github.com>
2023-01-27 11:45:03 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CSK101-M1-L53
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-xeayta)
---
+++ script.js
@@ -0,0 +1,37 @@
+// Challenge 1.1:
+// Step 1: Create an object for storing your profile information. The object name can be "studentProfile". below are the keys which are to be added to the object, please add your values for the same:
+// studentName,
+// studentAge,
+// cohortName,
+// location,
+//hobbies.
+var studentProfile = {
+  studentName: 'Aparnaa k',
+  studentAge: 20,
+  cohortName: 'engineer',
+  location: 'coimbatore',
+  hobbies: 'dance',
+};
+console.log(studentProfile);
+// Step 2: Add more then one hobby in the student object.
+//Hint: Use Arrays
+studentProfile.hobbies = ['dance', 'singing'];
+// Your code goes here
+console.log(studentProfile.hobbies);
+console.log(studentProfile);
+// Challenge 1.2: Access the studentName value of the object "studentProfile" using dot "." notation and print the same in console.
+
+var x = studentProfile.studentName;
+console.log(x);
+//Challenge 1.3: Access the cohortName property of the object using Bracket notation ([]).
+studentProfile['cohortName'];
+//Challenge 2.1:Create an object using the constructor function for storing the book information. The object name can be "Book". Include the following values .
+//title
+//author
+var book = {
+  title: 'kalvium',
+  author: 'Venkat',
+};
+//Challenge 2.2: Access the title property of the object "Book" and print the same in console.
+var w = book.title;
+console.log(w);
